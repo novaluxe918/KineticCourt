35724f1caaa72d74d5a83afacc48b6736648fb01
PhanMaiHoaiNhi <hoainhi8999@gmail.com>
2026-07-22 12:15:59 +0700

    summary schedules

--- sportfields/src/main/resources/static/js/facility-filter.js
+++ sportfields/src/main/resources/static/js/facility-filter.js
@@ -1,45 +1,65 @@
 const facility = document.getElementById("facility");
 const court = document.getElementById("court");
 
+const summaryFacility = document.getElementById("summaryFacility");
+const summaryCourt = document.getElementById("summaryCourt");
 
+
+// Chọn cơ sở
 facility.addEventListener("change", function () {
 
     let facilityId = this.value;
 
-
     let options = court.querySelectorAll("option");
 
 
+    // Hiển thị tên cơ sở
+    if(this.value === ""){
+        summaryFacility.innerText = "Chưa chọn";
+    }
+    else {
+        summaryFacility.innerText =
+            this.options[this.selectedIndex].text;
+    }
+
+
+    // Lọc sân theo cơ sở
     options.forEach(option => {
 
-
-        // option mặc định
         if(option.value === ""){
-
             option.style.display = "block";
             return;
-
         }
 
 
-        let courtFacility =
-            option.dataset.facility;
-
-
-        if(courtFacility === facilityId){
-
+        if(option.dataset.facility === facilityId){
             option.style.display = "block";
-
-        }else{
-
+        }
+        else {
             option.style.display = "none";
-
         }
 
     });
 
 
-    // reset sân đã chọn
+    // Reset sân khi đổi cơ sở
     court.value = "";
+
+    summaryCourt.innerText = "Chưa chọn";
+
+});
+
+
+
+// Chọn sân
+court.addEventListener("change", function () {
+
+    if(this.value === ""){
+        summaryCourt.innerText = "Chưa chọn";
+    }
+    else {
+        summaryCourt.innerText =
+            this.options[this.selectedIndex].text;
+    }
 
 });
--- sportfields/src/main/resources/static/js/time_slot.js
+++ sportfields/src/main/resources/static/js/time_slot.js
@@ -1,25 +1,29 @@
 
 let slotIndex = 1;
+
 function addTimeSlot(){
+tinhavg ();
    let container =
       document.getElementById("time-slots-container");
+      const summarySlot = document. getElementById("summarySlot");
+      summarySlot.innerText = parseInt(summarySlot.innerText) + 1;
    let html =` <div class="time-slot grid grid-cols-12 gap-4 items-end p-4 bg-surface-container-lowest border border-outline-variant rounded-lg transition-all hover:border-primary-container/50">
                                           <div class="col-span-4 space-y-2">
                                               <label class="block font-label-md text-label-md text-secondary">Giờ bắt đầu</label>
                                               <div class="relative">
-                                                  <input name="scheduleDetails[${slotIndex}].time_start" class="w-full p-3 bg-white border border-outline-variant rounded-lg font-body-md text-body-md focus:ring-2 focus:ring-primary-container focus:outline-none" type="time" />
+                                                  <input name="scheduleDetails[${slotIndex}].time_start" class="w-full p-3 bg-white border border-outline-variant rounded-lg font-body-md text-body-md focus:ring-2 focus:ring-primary-container focus:outline-none" type="time" required />
                                               </div>
                                           </div>
                                           <div class="col-span-4 space-y-2">
                                               <label class="block font-label-md text-label-md text-secondary">Giờ kết thúc</label>
                                               <div class="relative">
-                                                  <input name="scheduleDetails[${slotIndex}].time_end" class="w-full p-3 bg-white border border-outline-variant rounded-lg font-body-md text-body-md focus:ring-2 focus:ring-primary-container focus:outline-none" type="time" />
+                                                  <input name="scheduleDetails[${slotIndex}].time_end" class="w-full p-3 bg-white border border-outline-variant rounded-lg font-body-md text-body-md focus:ring-2 focus:ring-primary-container focus:outline-none" type="time" required />
                                               </div>
                                           </div>
                                           <div class="col-span-3 space-y-2">
                                               <label class="block font-label-md text-label-md text-secondary">Đơn giá (1h)</label>
                                               <div class="relative">
-                                                  <input name="scheduleDetails[${slotIndex}].price" class="w-full p-3 pr-12 bg-white border border-outline-variant rounded-lg font-body-md text-body-md focus:ring-2 focus:ring-primary-container focus:outline-none text-right" type="number" />
+                                                  <input  name="scheduleDetails[${slotIndex}].price" class="w-full price-input p-3 pr-12 bg-white border border-outline-variant rounded-lg font-body-md text-body-md focus:ring-2 focus:ring-primary-container focus:outline-none text-right" value="0" type="number" required />
                                                   <span class="absolute right-3 top-1/2 -translate-y-1/2 text-secondary text-xs font-bold">VND</span>
                                               </div>
                                           </div>
@@ -45,6 +49,8 @@ document.addEventListener("click", function (e) {
 
     const deleteButton = e.target.closest(".delete-slot");
 
+
+
     if (!deleteButton) {
         return;
     }
@@ -57,5 +63,45 @@ document.addEventListener("click", function (e) {
     }
 
     deleteButton.closest(".time-slot").remove();
-
+     summarySlot.innerText = parseInt(summarySlot.innerText) - 1;
+    updateSlotIndex();
 });
+
+function updateSlotIndex(){
+
+    const slots = document.querySelectorAll(".time-slot");
+
+    slots.forEach((slot,index)=>{
+
+        const inputs = slot.querySelectorAll("input");
+
+        inputs.forEach(input=>{
+
+            input.name = input.name.replace(
+                /\[\d+\]/,
+                `[${index}]`
+            );
+
+        });
+
+    });
+
+    slotIndex = slots.length;
+
+}
+// tong
+function tinhavg () {
+
+    const priceInput = document.querySelectorAll(".price-input");
+    // gan lai
+console.log(priceInput);
+priceInput.forEach(p => {
+ p.addEventListener("change", () => {
+   console.log(priceInput)
+    const summaryPrice = document.getElementById("summaryPrice");
+//    const avg = priceInput.reduce((sum, item) => sum + item.value, 0)/ priceInput.length;
+//    summaryPrice.innerText = avg;
+
+ })
+})
+}
